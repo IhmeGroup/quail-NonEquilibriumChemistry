231302f241e4963c0181c06c6b9ebebcfdec5d1c
Tim E. Dawson <tim.dawson@cfd-research.com>
2024-05-22 13:04:59 -0500

    Automatically discover submodules when installing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    description='Quail is a lightweight, open-source discontinuous Galerkin code written in Python for teaching and prototyping. Currently, Quail solves first-order nonlinear hyperbolic systems of partial differential equations.',
    author='Eric Ching, Brett Bornhoft, and Ali Lasemi',
    author_email='bornhoft@stanford.edu',
-   packages=['quail'],  #same as name
+   packages=setuptools.find_namespace_packages(),  #automatically discover submodules
    install_requires=['numpy', 'scipy', 'matplotlib'], #external packages as dependencies
 
    entry_points={
