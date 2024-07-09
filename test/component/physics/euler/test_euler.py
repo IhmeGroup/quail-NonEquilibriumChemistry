2d5e56ec4a0a3d88c6c9bdcffd43e1d6a53b266c
Jared M. Magnusson <jared.magnusson@cfd-research.com>
2024-07-09 16:13:16 -0500

    WIP fix-tests: update test_euler to handle get_conv_flux_interior new return values

--- test/component/physics/euler/test_euler.py
+++ test/component/physics/euler/test_euler.py
@@ -6,7 +6,6 @@ import quail.physics.base.thermo as thermo
 rtol = 1e-15
 atol = 1e-15
 
-
 def test_convective_flux_1D():
 	'''
 	This tests the convective flux for a 1D case.
@@ -16,32 +15,32 @@ def test_convective_flux_1D():
 
 	ns = physics.NUM_STATE_VARS
 
-	P = 101325.
-	rho = 1.1
-	u = 2.5
+	Pref = 101325.
+	rho_ref = 1.1
+	uref = 2.5
 	gamma = 1.4
-	rhoE = P / (gamma - 1.) + 0.5 * rho * u * u
+	rhoE = Pref / (gamma - 1.) + 0.5 * rho_ref * uref * uref
 
 	Uq = np.zeros([1, 1, ns])
 
 	irho, irhou, irhoE = physics.get_state_indices()
 
-	Uq[:, :, irho] = rho
-	Uq[:, :, irhou] = rho * u
+	Uq[:, :, irho] = rho_ref
+	Uq[:, :, irhou] = rho_ref * uref
 	Uq[:, :, irhoE] = rhoE
 
 
 	Fref = np.zeros([1, 1, ns, 1])
-	Fref[:, :, irho, 0] = rho * u
-	Fref[:, :, irhou, 0] = rho * u * u + P
-	Fref[:, :, irhoE, 0] = (rhoE + P) * u
+	Fref[:, :, irho, 0] = rho_ref * uref
+	Fref[:, :, irhou, 0] = rho_ref * uref * uref + Pref
+	Fref[:, :, irhoE, 0] = (rhoE + Pref) * uref
 
 	physics.set_physical_params()
-	F, (u2c, _, rhoc, pc) = physics.get_conv_flux_interior(Uq)
+	F, (u, thermo_obj) = physics.get_conv_flux_interior(Uq)
 
-	np.testing.assert_allclose(rhoc, rho, rtol, atol)	
-	np.testing.assert_allclose(u2c, u*u, rtol, atol)	
-	np.testing.assert_allclose(pc, P, rtol, atol)	
+	np.testing.assert_allclose(thermo_obj.rho, rho_ref, rtol, atol)	
+	np.testing.assert_allclose(u, uref, rtol, atol)	
+	np.testing.assert_allclose(thermo_obj.p, Pref, rtol, atol)	
 	np.testing.assert_allclose(F, Fref, rtol, atol)
 
 
@@ -53,31 +52,31 @@ def test_convective_flux_1D_zero_velocity():
 
 	ns = physics.NUM_STATE_VARS
 
-	P = 101325.
-	rho = 1.1
+	Pref = 101325.
+	rho_ref = 1.1
 	gamma = 1.4
-	rhoE = P / (gamma - 1.)
+	rhoE = Pref / (gamma - 1.)
 
 	Uq = np.zeros([1, 1, ns])
 
 	irho, irhou, irhoE = physics.get_state_indices()
 
-	Uq[:, :, irho] = rho
+	Uq[:, :, irho] = rho_ref
 	Uq[:, :, irhou] = 0.
 	Uq[:, :, irhoE] = rhoE
 
 
 	Fref = np.zeros([1, 1, ns, 1])
 	Fref[:, :, irho, 0] = 0.	
-	Fref[:, :, irhou, 0] = P
+	Fref[:, :, irhou, 0] = Pref
 	Fref[:, :, irhoE, 0] = 0.
 
 	physics.set_physical_params()
-	F, (u2c, _, rhoc, pc) = physics.get_conv_flux_interior(Uq)
+	F, (u, thermo_obj) = physics.get_conv_flux_interior(Uq)
 
-	np.testing.assert_allclose(rhoc, rho, rtol, atol)	
-	np.testing.assert_allclose(u2c, 0., rtol, atol)	
-	np.testing.assert_allclose(pc, P, rtol, atol)	
+	np.testing.assert_allclose(thermo_obj.rho, rho_ref, rtol, atol)	
+	np.testing.assert_allclose(u, 0., rtol, atol)	
+	np.testing.assert_allclose(thermo_obj.p, Pref, rtol, atol)	
 	np.testing.assert_allclose(F, Fref, rtol, atol)
 
 
@@ -91,37 +90,37 @@ def test_convective_flux_2D():
 
 	ns = physics.NUM_STATE_VARS
 
-	P = 101325.
-	rho = 1.1
-	u = 2.5
-	v = 3.5
+	Pref = 101325.
+	rho_ref = 1.1
+	uref = 2.5
+	vref = 3.5
 	gamma = 1.4
-	rhoE = P / (gamma - 1.) + 0.5 * rho * (u * u + v * v)
+	rhoE = Pref / (gamma - 1.) + 0.5 * rho_ref * (uref * uref + vref * vref)
 
 	Uq = np.zeros([1, 1, ns])
 
 	srho, srhou, srhoE = physics.get_state_slices()
 
-	Uq[:, :, srho] = rho
-	Uq[:, :, srhou] = [rho * u, rho*v]
+	Uq[:, :, srho] = rho_ref
+	Uq[:, :, srhou] = [rho_ref * uref, rho_ref*vref]
 	Uq[:, :, srhoE] = rhoE
 
 
 	Fref = np.zeros([1, 1, ns, 2])
-	Fref[:, :, srho, 0] = rho * u
-	Fref[:, :, srhou, 0] = [rho * u * u + P, rho * u * v]
-	Fref[:, :, srhoE, 0] = (rhoE + P) * u
+	Fref[:, :, srho, 0] = rho_ref * uref
+	Fref[:, :, srhou, 0] = [rho_ref * uref * uref + Pref, rho_ref * uref * vref]
+	Fref[:, :, srhoE, 0] = (rhoE + Pref) * uref
 
-	Fref[:, :, srho, 1] = rho * v
-	Fref[:, :, srhou, 1] = [rho * u * v, rho * v * v + P]
-	Fref[:, :, srhoE, 1] = (rhoE + P) * v
+	Fref[:, :, srho, 1] = rho_ref * vref
+	Fref[:, :, srhou, 1] = [rho_ref * uref * vref, rho_ref * vref * vref + Pref]
+	Fref[:, :, srhoE, 1] = (rhoE + Pref) * vref
 
 	physics.set_physical_params()
-	F, (u2c, _, rhoc, pc) = physics.get_conv_flux_interior(Uq)
+	F, (u, thermo_obj) = physics.get_conv_flux_interior(Uq)
 
-	np.testing.assert_allclose(rhoc, rho, rtol, atol)	
-	np.testing.assert_allclose(u2c, u*u+v*v, rtol, atol)	
-	np.testing.assert_allclose(pc, P, rtol, atol)	
+	np.testing.assert_allclose(thermo_obj.rho, rho_ref, rtol, atol)	
+	np.testing.assert_allclose(u.flatten(), np.array([uref, vref]), rtol, atol)	
+	np.testing.assert_allclose(thermo_obj.p, Pref, rtol, atol)	
 	np.testing.assert_allclose(F, Fref, rtol, atol)
 
 
@@ -133,34 +132,34 @@ def test_convective_flux_2D_zero_velocity():
 
 	ns = physics.NUM_STATE_VARS
 
-	P = 101325.
-	rho = 1.1
+	Pref = 101325.
+	rho_ref = 1.1
 	gamma = 1.4
-	rhoE = P / (gamma - 1.)
+	rhoE = Pref / (gamma - 1.)
 
 	Uq = np.zeros([1, 1, ns])
 
 	srho, srhou, srhoE = physics.get_state_slices()
 
-	Uq[:, :, srho] = rho
+	Uq[:, :, srho] = rho_ref
 	Uq[:, :, srhou] = np.array([0., 0.])
 	Uq[:, :, srhoE] = rhoE
 
 
 	Fref = np.zeros([1, 1, ns, 2])
 	Fref[:, :, srho, 0] = 0.
-	Fref[:, :, srhou, 0] = np.array([P, 0.])
+	Fref[:, :, srhou, 0] = np.array([Pref, 0.])
 	Fref[:, :, srhoE, 0] = 0.
 	Fref[:, :, srho, 1] = 0.
-	Fref[:, :, srhou, 1] = np.array([0., P])
+	Fref[:, :, srhou, 1] = np.array([0., Pref])
 	Fref[:, :, srhoE, 1] = 0.
 
 	physics.set_physical_params()
-	F, (u2c, _, rhoc, pc) = physics.get_conv_flux_interior(Uq)
+	F, (u, thermo_obj) = physics.get_conv_flux_interior(Uq)
 
-	np.testing.assert_allclose(rhoc, rho, rtol, atol)	
-	np.testing.assert_allclose(u2c, 0., rtol, atol)	
-	np.testing.assert_allclose(pc, P, rtol, atol)	
+	np.testing.assert_allclose(thermo_obj.rho, rho_ref, rtol, atol)	
+	np.testing.assert_allclose(u, 0., rtol, atol)	
+	np.testing.assert_allclose(thermo_obj.p, Pref, rtol, atol)	
 	np.testing.assert_allclose(F, Fref, rtol, atol)
 
 
